684105706342d7fb3bc98badef45b4ed87c735bc
SravaniDevops24 <sravanimaddula542@gmail.com>
2024-07-19 18:06:31 +0530

    expense-terraform-dev

--- 04-db/main.tf
+++ 04-db/main.tf
@@ -62,3 +62,23 @@ module "db" {
     },
   ]
 }
+
+# create route53 record for rds endpoint 
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 3.0"
+
+  zone_name = var.zone_name
+  records = [
+    {
+      name    = "db"
+      type    = "CNAME"
+      ttl = 1
+      records = [
+        module.db.db_instance_address
+      ]
+    }
+  ]
+
+}
--- 05-apps/data.tf
+++ 05-apps/data.tf
@@ -18,6 +18,11 @@ data "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 }
 
+#### ansible and already in frontend create public_subnet_id that can used 
+data "aws_ssm_parameter" "ansible_sg_id" {
+  name = "/${var.project_name}/${var.environment}/ansible_sg_id"
+}
+
 data "aws_ami" "ami_info" {
     most_recent = true
     owners = ["973714476881"]
